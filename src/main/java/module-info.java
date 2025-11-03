1aa83b48eda5369094171ebf8ee2faf4ad9fca35
Gleb <tquicklyy@yandex.ru>
2025-11-03 22:24:08 +0300

    Init commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.racewaykart.program.racewaykart {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.program.racewaykart to javafx.fxml;
+    exports com.program.racewaykart;
+}
