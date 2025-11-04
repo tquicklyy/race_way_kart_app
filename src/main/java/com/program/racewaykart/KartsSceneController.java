9bbe19b6a3a8a5713e280a44d28bf1b353958b78
Gleb <tquicklyy@yandex.ru>
2025-11-04 21:00:32 +0300

    Add drivers view #2

---
+++ src/main/java/com/program/racewaykart/KartsSceneController.java
@@ -0,0 +1,88 @@
+package com.program.racewaykart;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class KartsSceneController {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label addDataButton;
+
+    @FXML
+    private VBox addDataVBox;
+
+    @FXML
+    private HBox centerHBox;
+
+    @FXML
+    private VBox dataVBox;
+
+    @FXML
+    private VBox dataVBox1;
+
+    @FXML
+    private VBox generalVBox;
+
+    @FXML
+    private Label groupsButton;
+
+    @FXML
+    private Label groupsButton3111;
+
+    @FXML
+    private Label groupsButton31111;
+
+    @FXML
+    private HBox headerButtonsHBox;
+
+    @FXML
+    private HBox headersScrollPaneHBox;
+
+    @FXML
+    private HBox headersScrollPaneHBox1;
+
+    @FXML
+    private Label kartsButton;
+
+    @FXML
+    private ImageView logoImage;
+
+    @FXML
+    private TextField nameInput;
+
+    @FXML
+    private TextField patronymicInput;
+
+    @FXML
+    private Label resetAllButton;
+
+    @FXML
+    private Label resetDriversButton;
+
+    @FXML
+    private Label saveSerialButton;
+
+    @FXML
+    private TextField surnameInput;
+
+    @FXML
+    private HBox upperHBox;
+
+    @FXML
+    void initialize() {
+
+    }
+
+}
--- src/main/java/com/program/racewaykart/RaceWayKartApplication.java
+++ src/main/java/com/program/racewaykart/RaceWayKartApplication.java
@@ -3,18 +3,30 @@ package com.program.racewaykart;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class RaceWayKartApplication extends Application {
+
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(RaceWayKartApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage appStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(RaceWayKartApplication.class.getResource("karts-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        appStage.setTitle("RaceWayKart");
+
+        appStage.getIcons().clear();
+        appStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getResourceAsStream("/images/logo.png"))));
+        appStage.setResizable(false);
+
+        appStage.setScene(scene);
+
+        appStage.setOnCloseRequest(_ -> System.exit(0));
+
+        appStage.show();
     }
 
     public static void main(String[] args) {
